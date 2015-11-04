87e2eb9fff142d65fc23ca6bbd8d15d1de61e063
AVL13 <none@none>
2015-11-04 21:18:02 +0300

    Обход schema без рекурсии (печать результатов на экран).

--- p002.py
+++ p002.py
@@ -3,9 +3,37 @@ import collections
 
 
 def f(schema, data, arr_size=1, bit=0):
-    stack = [[schema, iter(schema), arr_size]]
-    while True:
-        cur_schema_item = next(stack[-1][1])
+    TOP = -1
+    ITER = 1
+    stack = []
+    if arr_size:
+        stack = [[schema, iter(schema), arr_size]]
+    #  2-й элемент списка для отслеживания текущей позиции на разных уровнях иерархии
+    #  если 3-й элемент (размер массива) отличен от 1, то 1-й элемент используется для
+    #  получения итератора во второй и последующие проходы.
+    names = ['/']
+    while stack:
+        try:
+            cur_schema_item = next(stack[TOP][ITER])
+
+            names.append(cur_schema_item[0])
+            if isinstance(cur_schema_item[1], int):
+                #  DO IT
+                print('.'.join(names), cur_schema_item[1], cur_schema_item[2])
+                names.pop()
+            else:
+                print('.'.join(names), cur_schema_item[2])
+                if cur_schema_item[2] != 0:
+                    stack.append([cur_schema_item[1],
+                                  iter(cur_schema_item[1]),
+                                  cur_schema_item[2]])
+        except StopIteration:
+            if stack[TOP][2] > 1:
+                stack[TOP][2] -= 1
+                stack[TOP][ITER] = iter(stack[TOP][0])
+            else:
+                stack.pop()
+                names.pop()
 
 
 if __name__ == '__main__':
@@ -14,3 +42,5 @@ if __name__ == '__main__':
               ('d', 2, 2), ('e', 1, 0),
               ('f', (('fa', 2, 1),), 1),
               ('g', (('ga', 2, 1), ('gb', (('gba', (('gbaa', 2, 2),), 1),), 2)), 1))
+
+    f(schema, None)
